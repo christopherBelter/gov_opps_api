52c77e537e51906ffce4c47697771e6cb530ef0b
Christopher Belter <christopher.belter@nih.gov>
2025-09-04 10:33:47 -0400

    Add files via upload

--- gov_opps_api.r
+++ gov_opps_api.r
@@ -1,115 +1,190 @@
-## required packages: httr and jsonlite
+# API documentation at https://grants.gov/api/
+## NIH Guide going away: https://grants.nih.gov/grants/guide/notice-files/NOT-OD-25-143.html
 
-get_nih_guide <- function(activitycodes = "all", doctype = "all", parentfoa = "all", daterange = "all", clinicaltrials = "all", fields = "all", spons = "true", parentic = "all", primaryic = "all", type = "active,expired,notices,activenosis", query = "", from = 0, perpage = 25, sort = "reldate:desc", outfile = "") {
-	## set up options and create containers for the retrieved data
-	base_url <- "https://search.grants.nih.gov/guide/api/data"
-	if (daterange == "all") {
-		daterange <- paste("01011991", format(Sys.Date(), "%m%d%Y"), sep = "-")
+create_query <- function(keyword = "", oppNum = "", eligibilities = "", agencies = "", oppStatuses = "", aln = "", fundingCategories = "", fundingInstruments = "", rows = 25, startRecordNum = 0, cfda = "", sortBy = "") {
+	the_q <- list(
+		rows = jsonlite::unbox(rows), ## number of results per request
+		keyword = jsonlite::unbox(keyword), ## text search box
+		oppNum = jsonlite::unbox(oppNum), ## opportunity number
+		eligibilities = jsonlite::unbox(eligibilities), ## what kind of entity is eligible to apply (i.e. county givernments, private institutions of higher education, etc)
+		agencies = jsonlite::unbox(agencies), ## top level agencies like HHS; NIH = HHS-NIH11
+		oppStatuses = jsonlite::unbox(oppStatuses), ## forecasted, posted, closed, archived; can search multiple by doing forecasted|posted
+			## forecasted = forecast, posted = currently accepting applications, archived = application period closed
+		aln = jsonlite::unbox(aln), ## assistance listing numbers? appears equivalent to cfda
+		fundingCategories = jsonlite::unbox(fundingCategories), ## funding activity categories: Health, Education, Environment, Food and Nutrition, Income Secutiry and Social Services
+		fundingInstruments = jsonlite::unbox(fundingInstruments), ## grant, cooperative agreement, procurement contract, other
+		startRecordNum = jsonlite::unbox(startRecordNum), 
+		cfda = jsonlite::unbox(cfda), ## equivalent of IC search; NICHD is 93.865,
+		#dateRangeOptions = jsonlite::unbox(dateRangeOptions), search seems to be ignoring this
+		sortBy = jsonlite::unbox(sortBy) ## I have no idea what the available sorting options are or what they're called
+		## there have got to be date range options available somewhere
+	)
+	the_q <- the_q[the_q != ""]
+	the_q <- jsonlite::toJSON(the_q)
+	return(the_q)
+}
+
+search_opps <- function(mq, start_number = 0, max_results = Inf) {
+	theData <- list()
+	the_response <- httr::POST("https://api.grants.gov/v1/api/search2", body = mq)
+	the_response <- httr::content(the_response, as = "text")
+	theData[[1]] <- jsonlite::fromJSON(the_response)
+	total_results <- theData[[1]]$data$hitCount
+	if (total_results == 0) {
+		message("No results found")
+		return(NA)
 	}
-	the_json <- list()
-	the_data <- list()
-	## get the first page of results, process the returned data, and see if additional pages are necessary
-	theURL <- httr::GET(base_url, query = list(activitycodes = activitycodes, doctype = doctype, parentfoa = parentfoa, daterange = daterange, clinicaltrials = clinicaltrials, fields = fields, spons = spons, parentic = parentic, primaryic = primaryic, type = type, query = query, from = from, perpage = perpage, sort = sort))
-		if (httr::http_error(theURL) == TRUE) { 
-			print("Encountered an HTTP error. Details follow.") 
-			print(httr::http_status(theURL)) 
-			break
-		}
-	the_json[[1]] <- httr::content(theURL, as = "text")
-	the_data[[1]] <- jsonlite::fromJSON(the_json[[1]])
-	num_results <- the_data[[1]]$data$hits$total
-	if (num_results == 0) { return(NA) }
-	pages_needed <- ceiling(num_results / perpage)
-	message(paste("Need", pages_needed, "pages"))
-	## loop for additional pages of results, if necessary
+	results_retrieved <- nrow(theData[[1]]$data$oppHits)
+	pages_needed <- ceiling(total_results / results_retrieved)
+	max_pages_needed <- ceiling(max_results / results_retrieved)
+	if (max_pages_needed < pages_needed) {
+		pages_needed <- max_pages_needed
+	}
+	theData[[1]] <- theData[[1]]$data$oppHits
+	message(paste("Found", total_results, "results.  Retrieving", pages_needed, "total batches"))
 	if (pages_needed > 1) {
 		for (i in 2:pages_needed) {
 			Sys.sleep(1)
-			from <- from + perpage
-			message(paste("Getting page", i, "of", pages_needed))
-			theURL <- httr::GET(base_url, query = list(activitycodes = activitycodes, doctype = doctype, parentfoa = parentfoa, daterange = daterange, clinicaltrials = clinicaltrials, fields = fields, spons = spons, parentic = parentic, primaryic = primaryic, type = type, query = query, from = from, perpage = perpage, sort = sort))
-				if (httr::http_error(theURL) == TRUE) { 
-					print("Encountered an HTTP error. Details follow.") 
-					print(httr::http_status(theURL)) 
-					break
-				}
-			the_json[[i]] <- httr::content(theURL, as = "text")
-			the_data[[i]] <- jsonlite::fromJSON(the_json[[i]])
+			last_start <- start_number
+			start_number <- start_number + results_retrieved
+			mq <- gsub(paste0("\"startRecordNum\":", last_start), paste0("\"startRecordNum\":", start_number), mq)
+			the_response <- httr::POST("https://api.grants.gov/v1/api/search2", body = mq)
+			the_response <- httr::content(the_response, as = "text")
+			theData[[i]] <- jsonlite::fromJSON(the_response)
+			results_retrieved <- nrow(theData[[i]]$data$oppHits)
+			theData[[i]] <- theData[[i]]$data$oppHits
 		}
-	}		
-	## save the JSON results to the outfile, if one is specified
-	if (outfile != "") {
-		writeLines(unlist(the_json), con = outfile)
-	}	
-	## reshape the JSON into a usable data frame and then return it
-	the_data <- extract_guide(the_data, the_source = "api")
-	return(the_data)
-	#return(the_json)
+		theData <- do.call(rbind, theData)
+	}
+	else if (pages_needed == 1) {
+		theData <- theData[[1]]
+	}
+	theData$cfdaList <- sapply(theData$cfdaList, paste, collapse = ";")
+	return(theData)
 }
 
-extract_guide <- function(x, the_source = "file") {
-	expected_cols <- c("rowid", "type", "title", "docnum", "parentIC", "primaryIC", "reldate", "expdate", "parentFOA", "doctype", "ac", "filename", "clinicaltrials", "sponsors", "noticeSubject", "urgetFoaMother", "opendate", "appreceiptdate", "lard", "intentdate", "fadirectcosts", "purpose", "ApplicationType", "relatedDocs", "doctext", "policycategory", "organization.parent", "organization.primary")
-	if (the_source == "file") {
-		the_data <- scan(x, what = "varchar", sep = "\n", quiet = TRUE)
-		the_data <- lapply(the_data, jsonlite::fromJSON)
+fetch_opps <- function(the_opp_ids) {
+	the_pages <- list()
+	for (i in 1:length(the_opp_ids)) {
+		mq <- paste0("{\"opportunityId\":", the_opp_ids[i], "}")
+		the_response <- httr::POST("https://api.grants.gov/v1/api/fetchOpportunity", body = mq)
+		the_response <- httr::content(the_response, as = "text")
+		the_response <- jsonlite::fromJSON(the_response)
+		the_response <- the_response$data
+		the_response[sapply(the_response, length) == 0] <- NA
+		the_response <- as.data.frame(t(unlist(the_response)))
+		dup_cols <- colnames(the_response)[grepl("\\d$", colnames(the_response))]
+		dup_cols <- unique(gsub("\\d{1,2}$", "", dup_cols)) ## could there be over 100 duplicate columns?
+		new_cols <- sapply(1:length(dup_cols), function(x) paste(unique(unlist(unname(the_response[,grepl(dup_cols[x], colnames(the_response))]))), collapse = ";"))
+		names(new_cols) <- dup_cols
+		new_cols <- as.data.frame(t(new_cols))
+		the_response <- cbind(the_response, new_cols)
+		the_response <- the_response[,grepl("\\d$", colnames(the_response)) == FALSE]
+		the_pages[[i]] <- the_response
+		Sys.sleep(1)
 	}
-	else if (the_source == "api") {
-		the_data <- x
-	}
-	else { 
-		message("Invalid the_source argument. Valid responses are 'file' and 'api'")
-		break
-	}
-	for (i in 1:length(the_data)) {
-		the_data[[i]] <- jsonlite::flatten(the_data[[i]]$data$hits$hits)
-		colnames(the_data[[i]]) <- gsub("_source.|_", "", colnames(the_data[[i]]))
-		the_data[[i]] <- the_data[[i]][,colnames(the_data[[i]]) %in% c("index", "id", "score", "sort", "ignored", "suggest.input", "highlight.primaryIC", "highlight.ac", "highlight.docnum") == FALSE]
-		the_data[[i]] <- the_data[[i]][,grepl("highlight\\.", colnames(the_data[[i]])) == FALSE]
-		list_cols <- which(sapply(1:ncol(the_data[[i]]), function(x) is.list(the_data[[i]][,x])))
-		for (j in 1:length(list_cols)) {
-		  the_data[[i]][,list_cols[j]] <- sapply(the_data[[i]][,list_cols[j]], paste, collapse = ";")
-		}
-		if (any(expected_cols %in% colnames(the_data[[i]]) == FALSE)) {
-			pcols <- expected_cols[expected_cols %in% colnames(the_data[[i]]) == FALSE]
-			the_data[[i]][,pcols] <- ""
-			the_data[[i]] <- the_data[[i]][,expected_cols]
-		}	
-	}
-	the_data <- do.call(plyr::rbind.fill, the_data)
-	the_data$title <- gsub("\\s+?", " ", the_data$title)
-	the_data$purpose <- gsub("\\s+?", " ", the_data$purpose)
-	the_data$nofo_url[grepl("^PA", the_data$doctype)] <- paste0("https://grants.nih.gov/grants/guide/pa-files/", the_data$filename[grepl("^PA", the_data$doctype)])
-	the_data$nofo_url[grepl("^RFA", the_data$doctype)] <- paste0("https://grants.nih.gov/grants/guide/rfa-files/", the_data$filename[grepl("^RFA", the_data$doctype)])
-	the_data$nofo_url[grepl("^NOT", the_data$doctype)] <- paste0("https://grants.nih.gov/grants/guide/notice-files/", the_data$filename[grepl("^NOT", the_data$doctype)])
-	return(the_data)
-}
+	the_pages <- do.call(plyr::rbind.fill, the_pages) ## need to work on this
+	return(the_pages)
+} ## note: still getting number columns in the result; i.e. opportunityHistoryDetails.synopsis.fundingInstruments.id.1
 
-get_related_nofos <- function(nofo_num) {
-	current_foa <- nofo_num
-	curr_doc <- get_nih_guide(query = paste0("\"", current_foa, "\""))
-	the_docs <- curr_doc
-	rel_nofos <- curr_doc %>% 
-	  filter(parentFOA == "No") %>% 
-	  select(relatedDocs) %>% 
-	  separate_rows(relatedDocs, sep = ";") %>% 
-	  filter(grepl("RFA|PA", relatedDocs)) %>% 
-	  mutate(relatedDocs = gsub(".+ ", "", relatedDocs)) %>% 
-	  unique() %>% 
-	  filter(relatedDocs %in% the_docs$docnum == FALSE, grepl("-\\d{3}", relatedDocs))
-	Sys.sleep(1)
-	while(nrow(rel_nofos) > 0) {
-	curr_doc <- get_nih_guide(query = paste0("\"", rel_nofos$relatedDocs, "\"", collapse = " OR "))
-	the_docs <- the_docs %>% bind_rows(curr_doc) %>% unique()
-	rel_nofos <- curr_doc %>% 
-	  filter(parentFOA == "No") %>% 
-	  select(relatedDocs) %>% 
-	  separate_rows(relatedDocs, sep = ";") %>% 
-	  filter(grepl("RFA|PA", relatedDocs)) %>% 
-	  mutate(relatedDocs = gsub(".+ ", "", relatedDocs)) %>% 
-	  unique() %>% 
-	  filter(relatedDocs %in% the_docs$docnum == FALSE, grepl("\\d{2}-\\d{3}", relatedDocs))
-	  Sys.sleep(1)
+simplify_opps <- function(the_opps, opp_type) {
+	if (opp_type %in% c("synopsis", "posted")) {
+	newDF <- the_opps[,c(
+		"id",
+		"docType",
+		"opportunityNumber",
+		"opportunityTitle",
+		"opportunityCategory.description",
+		paste0(opp_type, ".fundingInstruments.description"),
+		paste0(opp_type, ".fundingActivityCategories.description"),
+		"cfdas.cfdaNumber",
+		"cfdas.programTitle",
+		paste0(opp_type, ".costSharing"),
+		paste0(opp_type, ".version"),
+		paste0(opp_type, ".postingDate"),
+		paste0(opp_type, ".lastUpdatedDate"),
+		"originalDueDate",
+		"opportunityPkgs.openingDate", 
+		"opportunityPkgs.closingDate", 
+		paste0(opp_type, ".archiveDate"),
+		paste0(opp_type, ".awardFloor"),
+		paste0(opp_type, ".awardCeiling"),
+		paste0(opp_type, ".applicantTypes.description"),
+		paste0(opp_type, ".applicantEligibilityDesc"),
+		"agencyDetails.agencyName",
+		paste0(opp_type, ".synopsisDesc"),
+		paste0(opp_type, ".fundingDescLinkUrl"),
+		paste0(opp_type, ".agencyContactDesc"),
+		paste0(opp_type, ".agencyContactEmail"),
+		paste0(opp_type, ".agencyContactEmailDesc"),
+		"relatedOpps"
+	)
+	]
 	}
-	the_docs <- the_docs %>% filter(parentFOA == "No")
-	return(the_docs)
+	else if (opp_type == "archived") {
+		newDF <- the_opps[,c(
+		"id",
+		"docType",
+		"opportunityNumber",
+		"opportunityTitle",
+		"opportunityCategory.description",
+		"synopsis.fundingInstruments.description",
+		"synopsis.fundingActivityCategories.description",
+		"cfdas.cfdaNumber",
+		"cfdas.programTitle",
+		"synopsis.costSharing",
+		"synopsis.version",
+		"synopsis.postingDate",
+		"synopsis.lastUpdatedDate",
+		"originalDueDate",
+		"closedOpportunityPkgs.openingDate", 
+		"closedOpportunityPkgs.closingDate", 
+		"synopsis.archiveDate",
+		"synopsis.awardFloor",
+		"synopsis.awardCeiling",
+		"synopsis.applicantTypes.description",
+		"synopsis.applicantEligibilityDesc",
+		"agencyDetails.agencyName",
+		"synopsis.synopsisDesc",
+		"synopsis.fundingDescLinkUrl",
+		"synopsis.agencyContactDesc",
+		"synopsis.agencyContactEmail",
+		"synopsis.agencyContactEmailDesc",
+		"relatedOpps"
+	)
+	]
+	}
+	else if (opp_type == "forecast") {
+	newDF <- the_opps[,c(
+		"id",
+		"docType",
+		"opportunityNumber",
+		"opportunityTitle",
+		"opportunityCategory.description",
+		paste0(opp_type, ".fundingInstruments.description"),
+		paste0(opp_type, ".fundingActivityCategories.description"),
+		"cfdas.cfdaNumber",
+		"cfdas.programTitle",
+		paste0(opp_type, ".costSharing"),
+		paste0(opp_type, ".version"),
+		paste0(opp_type, ".postingDate"),
+		paste0(opp_type, ".lastUpdatedDate"),
+		paste0(opp_type, ".applicantTypes.description"),
+		paste0(opp_type, ".applicantEligibilityDesc"),
+		"agencyDetails.agencyName",
+		"forecast.forecastDesc",
+		"forecast.estSynopsisPostingDateStr",
+		"forecast.estApplicationResponseDateStr",
+		"forecast.estAwardDateStr",
+		"forecast.estProjectStartDateStr",
+		paste0(opp_type, ".agencyContactEmail"),
+		paste0(opp_type, ".agencyContactEmailDesc"),
+		"relatedOpps"
+	)
+	]
+	}
+	else {
+	stop("Invalid opp_type. Valid values are 'forecast', 'posted', 'synopsis', and 'archived'")
+	}
+	return(newDF)
 }
